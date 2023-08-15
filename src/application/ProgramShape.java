05df487eab2d4c5d3993fabdb0242db753bce50e
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-08-15 15:55:50 -0300

    Implementação com Herança e com Interface

--- src/application/ProgramShape.java
+++ src/application/ProgramShape.java
@@ -6,8 +6,8 @@ public class ProgramShape {
 
     public static void main(String[] args) {
 
-        Shape s1 = new Circle(Color.BLACK, 2.0);
-        Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+        AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+        AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 
         System.out.println("Cor do círculo: " + s1.getColor());
         System.out.println("Área do círculo: " + String.format("%.2f", s1.area()));
